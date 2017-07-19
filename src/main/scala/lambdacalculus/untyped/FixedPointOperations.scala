96945c7a363bf9fcb3773289c1e38c292ca3dcca
k_higuchi <koyohiguchi7788@gmail.com>
2017-07-20 01:10:42 +0900

    Yコンビネーターのコメント修正

--- src/main/scala/lambdacalculus/untyped/FixedPointOperations.scala
+++ src/main/scala/lambdacalculus/untyped/FixedPointOperations.scala
@@ -9,8 +9,8 @@ object FixedPointOperations {
 
   // Y  = λf.(λx.f(x x))(λx.f(x x))
   // Yg = λf.(λx.f(x x))(λx.f(x x))g  (Yの定義より)
-  // Yg = λx.g(x x))(λx.g(x x)        (λfのβ簡約、主関数をgに適用)
-  // Yg = λy.g(y y))(λx.g(x x)        (α変換、束縛変数の名前を変える)
+  // Yg = λx.g(x x)(λx.g(x x))        (λfのβ簡約、主関数をgに適用)
+  // Yg = λy.g(y y)(λx.g(x x))        (α変換、束縛変数の名前を変える)
   // Yg = g(λx.g(x x)(λx.g(x x)))     (λyのβ簡約、左の関数を右の関数に適用)
   // Yg = g(Yg)                       (第2式より)
   def Y[A, B]: YCombinator[A, B] = f => f(Y(f))
