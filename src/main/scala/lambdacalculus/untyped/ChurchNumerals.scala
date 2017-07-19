b12aa45c27753561707e8cc5f26da22f4ea07158
k_higuchi <koyohiguchi7788@gmail.com>
2017-07-19 22:45:35 +0900

    チャーチ数を追加

---
+++ src/main/scala/lambdacalculus/untyped/ChurchNumerals.scala
@@ -0,0 +1,18 @@
+package lambdacalculus.untyped
+
+/**
+  * Created by k_higuchi on 2017/07/16.
+  */
+object ChurchNumerals {
+
+  type ChurchNumeral = (Any => Any) => Any => Any
+
+  val Zero: ChurchNumeral = f => x => x
+  val One: ChurchNumeral = f => x => f(x)
+  val Five: ChurchNumeral = f => x => f(f(f(f(f(x)))))
+  val Fifteen: ChurchNumeral = f => x => f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(x)))))))))))))))
+  val Hundred: ChurchNumeral = f => x => f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(x))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+
+  def toInt(func: ChurchNumeral): Int = func(n => n.asInstanceOf[Int] + 1)(0).asInstanceOf[Int]
+
+}
